9ef1509c231f02ff918fab9270a2cef8cbc57860
oraiqatm <oraiqatm@mahmouds-MacBook-Pro.local>
2022-07-11 08:55:46 -0700

    added client

---
+++ socketProgram/src/socketProgram/Client.java
@@ -0,0 +1,36 @@
+package socketCommunication;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+	
+	private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws UnknownHostException, IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public static void main(String args[]) throws IOException{
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 4000);
+        String response = client.sendMessage("hello server");
+        response = client.sendMessage(".");
+        System.out.println(response);
+	}
+}
